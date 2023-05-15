a5b337b90a4f3269f2d90fd93432824fdec94cff
Daesh <85724168+sygyzyx@users.noreply.github.com>
2023-05-15 19:05:47 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HI TEXT ANIMATION 
 document.addEventListener('DOMContentLoaded', function() {
     const HIText = document.getElementById('HIText');
     const text = HIText.textContent
@@ -8,6 +9,66 @@ function changeText() {
     HIText.textContent = texts[currentIndex];
   currentIndex = (currentIndex + 1) % texts.length;
 }
-
 setInterval(changeText, 1000);
   });
+//
+
+// ABOUT ME ANIMATION 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+let lastScrollPosition = window.pageYOffset;
+
+document.addEventListener('scroll', function () {
+  const currentScrollPosition = window.pageYOffset;
+  const windowHeight = window.innerHeight;
+  const scrollThreshold = windowHeight * 0.7;
+
+  if (currentScrollPosition > lastScrollPosition) {
+    // Scrolling down
+    hiddenElements.forEach((el) => {
+      if (!el.classList.contains('show')) {
+        el.classList.add('show');
+      }
+    });
+  } else {
+    // Scrolling up
+    hiddenElements.forEach((el) => {
+      const rect = el.getBoundingClientRect();
+      const elementPosition = rect.top - scrollThreshold;
+      const isVisible = elementPosition <= 0 && rect.bottom >= 0;
+      if (isVisible) {
+        el.classList.add('show');
+      } else {
+        el.classList.remove('show');
+      }
+    });
+  }
+
+  lastScrollPosition = currentScrollPosition;
+});
+
+
+
+//
+
+
+// SCROLL DOWN ARROW ANIMATION 
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    var element = document.querySelector('.material-symbols-outlined');
+    element.style.opacity = '1';
+  }, 4000); // Delay in milliseconds (10 seconds)
+});
+//
+
